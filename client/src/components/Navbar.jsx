c188e8748b24a2985b51c32b600439d337130c29
Johan Janérs <johanjaners@gmail.com>
2025-11-25 16:15:27 +0100

    Add transparent-to-solid navbar with scroll detection

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 80)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${isScrolled ? 'navbar--scrolled' : ''}`}>
       <div className="navbar__brand">Movie / Book Library</div>
       <nav className="navbar__links">
         <NavLink to="/">Home</NavLink>
